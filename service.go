8a0e6dc594cab86b921129624488c8a9fcb2430e
Matthew Connelly <matthew.b.connelly@gmail.com>
2021-10-21 21:01:28 -0400

    initial json request success

---
+++ service.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type exchange struct {
+	Buy     string `json:"buy"`
+	Sell    string `json:"sell"`
+	BuyNow  bool   `json:"buyNow"`
+	SellNow bool   `json:"sellNow"`
+}
+
+type coinbaseSchema struct {
+	Data struct {
+		Base     string `json:"base"`
+		Currency string `json:"currency"`
+		Amount   string `json:"amount"`
+	} `json:"data"`
+}
+
+func getRespJSON(w http.ResponseWriter, url string, schema interface{}) ([]byte, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	json.NewDecoder(response.Body).Decode(schema)
+
+	j, err := json.Marshal(schema)
+	if err != nil {
+		return nil, err
+	}
+	return j, nil
+}
+
+func main() {
+	http.HandleFunc("/btc", func(w http.ResponseWriter, r *http.Request) {
+		response := &coinbaseSchema{}
+		j, err := getRespJSON(w, "https://api.coinbase.com/v2/prices/BTC-USD/buy", response)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(j)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
